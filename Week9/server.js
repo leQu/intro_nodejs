5c21cc11337a1c3f0c2a52d60c71449168ae3f1a
Alexander Lantz <alex.w.lantz@gmail.com>
2026-03-18 11:48:29 +0100

    week9

---
+++ Week9/server.js
@@ -0,0 +1,54 @@
+import express from "express";
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+import { Server } from "socket.io";
+
+/* SETUP EXPRESS SERVER */
+const app = express();
+const server = app.listen(3000, () =>
+  console.log("Server is running on port 3000"),
+);
+// ______________________
+
+/* SETUP FILE PATHS AND SERVE HTML DOCUMENT*/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+// ______________________
+
+/* SETUP FILE UPLOAD WITH MULTER */
+const upload = multer({
+  dest: "uploads/",
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === "application/pdf") {
+      cb(null, true);
+    } else {
+      cb(new Error("Invalid file type"), false);
+    }
+  },
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.send("File uploaded successfully");
+});
+// ______________________
+
+/* SETUP WEBSOCKET SERVER WITH SOCKET.IO */
+const io = new Server(server);
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("chatMessage", (msg) => {
+    console.log(msg);
+    socket.broadcast.emit("chatMessage", msg);
+    socket.emit("chatMessage", `You said: ${msg}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+// ______________________
